234287b6822c06772ebf8a536ff54ce190b5ea33
Umran Ozman <umonal@yahoo.com>
2022-09-03 23:19:58 -0400

    Electronic Class is made abstract

--- src/main/java/com/cydeo/category/Electronic.java
+++ src/main/java/com/cydeo/category/Electronic.java
@@ -1,9 +1,12 @@
 package com.cydeo.category;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
-public class Electronic extends Category{
+public abstract class Electronic extends Category{
     public Electronic(UUID id, String name) {
         super(id, name);
     }
+
+    public abstract LocalDate findDeliveryDueDate();
 }
