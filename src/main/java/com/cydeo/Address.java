04fd4e0fb128a88b64c2acb17bc31aefc4f3fe98
Umran Ozman <umonal@yahoo.com>
2022-09-03 20:44:18 -0400

    Adress Class fields are defined

--- src/main/java/com/cydeo/Address.java
+++ src/main/java/com/cydeo/Address.java
@@ -1,5 +1,9 @@
 package com.cydeo;
 
 public class Address {
-
+    private String streetNumber;
+    private String streetName;
+    private String additionalAddressLine;
+    private String zipCode;
+    private String state;
 }
