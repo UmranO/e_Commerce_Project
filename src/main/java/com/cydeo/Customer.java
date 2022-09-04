ecee4a99fb02dc8c4bc09792d6c2f4f1cd962328
Umran Ozman <umonal@yahoo.com>
2022-09-03 21:05:32 -0400

    Customer Class constructors (overloaded constructors-giving 2 way to create acct to customer--option)

--- src/main/java/com/cydeo/Customer.java
+++ src/main/java/com/cydeo/Customer.java
@@ -1,5 +1,22 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
 
+    private String userName;
+    private String email;
+    private List<Address> address;
+
+    public Customer(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public Customer(String userName, String email, List<Address> address) {
+        this.userName = userName;
+        this.email = email;
+        this.address = address;
+    }
 }
