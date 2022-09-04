b95b7395a630335a17d4437f6c20bfc43340c05e
Umran Ozman <umonal@yahoo.com>
2022-09-03 21:23:49 -0400

    Customer Class UUID field created and constructors generated again

--- src/main/java/com/cydeo/Customer.java
+++ src/main/java/com/cydeo/Customer.java
@@ -2,19 +2,24 @@ package com.cydeo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Customer {
-
+    private UUID id;              //UUID is a class that create a random UUID which is unique
+                                  //Customer Class needs a field from a Class already built in Java not from a class we created
+                                  // It's coming from a different class so it has to be imprted
     private String userName;
     private String email;
     private List<Address> address;
 
-    public Customer(String userName, String email) {
+    public Customer(UUID id, String userName, String email) {
+        this.id = id;
         this.userName = userName;
         this.email = email;
     }
 
-    public Customer(String userName, String email, List<Address> address) {
+    public Customer(UUID id, String userName, String email, List<Address> address) {
+        this.id = id;
         this.userName = userName;
         this.email = email;
         this.address = address;
