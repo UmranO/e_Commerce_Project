1f29e38668215c37f0750138a7433936b22adf2b
Umran Ozman <umonal@yahoo.com>
2022-09-03 20:41:15 -0400

    Main Class is defined

--- src/main/java/com/cydeo/Main.java
+++ src/main/java/com/cydeo/Main.java
@@ -1,4 +1,10 @@
 package com.cydeo;
 
 public class Main {
+       //createCustomer();
+       //createCategory();
+       //createProduct();
+       //createBalance();
+       //createDiscount();
+
 }
