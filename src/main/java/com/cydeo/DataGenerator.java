d00dc563b3d88dc2631fee56460455b919db1236
Umran Ozman <umonal@yahoo.com>
2022-09-03 21:57:56 -0400

    Address samples added to List of address for customer 1 /customer 1 and customer 2 are created

--- src/main/java/com/cydeo/DataGenerator.java
+++ src/main/java/com/cydeo/DataGenerator.java
@@ -1,15 +1,25 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class DataGenerator {     //DataGenerator Class is created for creating sample customers. Sample customers better not created in Main Class to keep it neet
-                                 //Plus I do have a method creating a customer for me
+    //Plus I do have a method creating a customer for me
 
 
-public static void createCustomer(){
+    public static void createCustomer() {
 
-    Address address1Customer1 = new Address("7925","Jones Branch Dr","Suite 3300","22102","VA");
-    Address address2Customer1 = new Address("825","GeorgeTown Pky","Suite 5355","22036","VA");
-    Address address1Customer2 = new Address("5924","Lee Hwy","House","22044","VA");
+        Address address1Customer1 = new Address("7925", "Jones Branch Dr", "Suite 3300", "22102", "VA");
+        Address address2Customer1 = new Address("825", "GeorgeTown Pky", "Suite 5355", "22036", "VA");
+        Address address1Customer2 = new Address("5924", "Lee Hwy", "House", "22044", "VA");
 
-}
 
+        List<Address> customer1AddressList = new ArrayList<>();
+        customer1AddressList.add(address1Customer1);
+        customer1AddressList.add(address2Customer1);
+
+        Customer customer1 = new Customer(UUID.randomUUID(),"ozzy","ozzy@cydeo.com",customer1AddressList);
+        Customer customer2 = new Customer(UUID.randomUUID(),"mike","mike@gmail.com");
+    }
 }
