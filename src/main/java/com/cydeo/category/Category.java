7fe6a5ea5d4ba029846fb33323b8cb4468c0ef46
Umran Ozman <umonal@yahoo.com>
2022-09-03 22:59:30 -0400

    Category Class constructor generated

--- src/main/java/com/cydeo/category/Category.java
+++ src/main/java/com/cydeo/category/Category.java
@@ -1,4 +1,16 @@
 package com.cydeo.category;
 
+import java.util.UUID;
+
 public class Category {
+
+    private UUID id;
+    private String name;
+
+    public Category(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
 }
