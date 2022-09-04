d7e1354e41a58a26446baff121e2ef20d2620d1f
Umran Ozman <umonal@yahoo.com>
2022-09-03 21:46:35 -0400

    Address samples created

--- src/main/java/com/cydeo/DataGenerator.java
+++ src/main/java/com/cydeo/DataGenerator.java
@@ -6,6 +6,9 @@ public class DataGenerator {     //DataGenerator Class is created for creating s
 
 public static void createCustomer(){
 
+    Address address1Customer1 = new Address("7925","Jones Branch Dr","Suite 3300","22102","VA");
+    Address address2Customer1 = new Address("825","GeorgeTown Pky","Suite 5355","22036","VA");
+    Address address1Customer2 = new Address("5924","Lee Hwy","House","22044","VA");
 
 }
 
