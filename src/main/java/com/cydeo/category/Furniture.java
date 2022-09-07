10e398aa0145722cdda47046b1e7ddf9b8288c61
Umran Ozman <umonal@yahoo.com>
2022-09-07 15:02:37 -0400

    findDeliveryDueDate() implementation given

--- src/main/java/com/cydeo/category/Furniture.java
+++ src/main/java/com/cydeo/category/Furniture.java
@@ -1,4 +1,21 @@
 package com.cydeo.category;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Furniture extends Category{
+
+
+    public Furniture(UUID id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public LocalDateTime findDeliveryDueDate() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return localDateTime.plusDays(1);
+    }
+
+
 }
