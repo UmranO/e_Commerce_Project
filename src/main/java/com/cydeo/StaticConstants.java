a0457938567729f7d880923a876ed9e2b3e65176
Umran Ozman <umonal@yahoo.com>
2022-09-07 16:10:38 -0400

    CATEGORY_LIST created in StaticConstants

---
+++ src/main/java/com/cydeo/StaticConstants.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import com.cydeo.category.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticConstants {
+    //We created this as a Database
+    public static final List<Customer> CUSTOMER_LIST = new ArrayList<>();
+    public static final List<Category> CATEGORY_LIST = new ArrayList<>();
+}
