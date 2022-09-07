11c865df08fcd5ce91e4d5d10f2a7c34e9c48e22
Umran Ozman <umonal@yahoo.com>
2022-09-07 16:36:45 -0400

    Instance Fields are defined

--- src/main/java/com/cydeo/Product.java
+++ src/main/java/com/cydeo/Product.java
@@ -1,4 +1,14 @@
 package com.cydeo;
 
+import java.util.UUID;
+
 public class Product {
+
+    private UUID id;
+    private String name;
+    private Double price;
+    private Integer stock;
+    private Integer remainingStock;
+    private UUID categoryId;
+
 }
