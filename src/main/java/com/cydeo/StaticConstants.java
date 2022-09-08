f8be5f1d9b55123758622b60c74b3fd9d6db39a4
Umran Ozman <umonal@yahoo.com>
2022-09-08 17:31:43 -0400

    GIFT_CARD_BALANCE_LIST created

--- src/main/java/com/cydeo/StaticConstants.java
+++ src/main/java/com/cydeo/StaticConstants.java
@@ -14,4 +14,6 @@ public class StaticConstants {
     public static final List<Product> PRODUCT_LIST = new ArrayList<>();
 
     public static final List<Balance> BALANCE_LIST=new ArrayList<>();
+
+    public static final List<Balance> GIFT_CARD_BALANCE_LIST=new ArrayList<>();
 }
