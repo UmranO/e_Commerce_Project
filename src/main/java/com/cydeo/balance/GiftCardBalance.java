13df5e9c49b3fac263b9df8ebc44166c4b83fdfb
Umran Ozman <umonal@yahoo.com>
2022-09-08 16:38:53 -0400

    GiftCardBalance Class Constructor

--- src/main/java/com/cydeo/balance/GiftCardBalance.java
+++ src/main/java/com/cydeo/balance/GiftCardBalance.java
@@ -1,4 +1,11 @@
 package com.cydeo.balance;
 
+import java.util.UUID;
+
 public class GiftCardBalance extends Balance{
+
+    public GiftCardBalance(UUID customerId, Double balance) {
+        super(customerId, balance);
+    }
+
 }
