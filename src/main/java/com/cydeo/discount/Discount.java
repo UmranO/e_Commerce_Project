9e7a11dafbc9ab864697c264931304bfe765bc30
Umran Ozman <umonal@yahoo.com>
2022-09-08 19:14:35 -0400

    Constructor/Getters created Discount Class

---
+++ src/main/java/com/cydeo/discount/Discount.java
@@ -0,0 +1,31 @@
+package com.cydeo.discount;
+
+import java.lang.reflect.Constructor;
+import java.util.UUID;
+
+public class Discount {
+
+    private UUID discountId;
+    private String name;
+    private Double thresholdAmount;
+
+    public Discount(UUID discountId, String name, Double thresholdAmount) {
+        this.discountId = discountId;
+        this.name = name;
+        this.thresholdAmount = thresholdAmount;
+    }
+
+    public UUID getDiscountId() {
+        return discountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getThresholdAmount() {
+        return thresholdAmount;
+    }
+
+
+}
