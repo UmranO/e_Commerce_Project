327a086caebd5a329eb765d539fc8641b0868b0c
Umran Ozman <umonal@yahoo.com>
2022-09-09 08:39:23 -0400

    Constructor, getter, abstract () created RateBasedDiscount Class

---
+++ src/main/java/com/cydeo/discount/RateBasedDiscount.java
@@ -0,0 +1,20 @@
+package com.cydeo.discount;
+
+import java.util.UUID;
+
+public class RateBasedDiscount extends Discount{
+
+    private Double rateAmount;
+
+    public RateBasedDiscount(UUID discountId, String name, Double thresholdAmount, Double rateAmount) {
+        super(discountId, name, thresholdAmount);
+        this.rateAmount = rateAmount;
+    }
+
+    @Override
+    public Double calculateCartAmountAfterDiscountApplied(Double amount) {
+        return null;
+    }
+
+
+}
