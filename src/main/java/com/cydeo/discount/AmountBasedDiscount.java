fcf7f552e44ca23ff51f3a86bd6778e81a6da933
Umran Ozman <umonal@yahoo.com>
2022-09-09 08:59:30 -0400

    unique field,Constructor, getter created AmountBasedDiscount Class

---
+++ src/main/java/com/cydeo/discount/AmountBasedDiscount.java
@@ -0,0 +1,23 @@
+package com.cydeo.discount;
+
+import java.util.UUID;
+
+public class AmountBasedDiscount extends Discount{
+
+    private Double discountAmount;
+
+
+    public AmountBasedDiscount(UUID discountId, String name, Double thresholdAmount, Double discountAmount) {
+        super(discountId, name, thresholdAmount);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    public Double calculateCartAmountAfterDiscountApplied(Double amount) {
+        return null;
+    }
+
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
+}
