cbf718845dcd1f1f3c3448cf37fa58274ee45e32
Umran Ozman <umonal@yahoo.com>
2022-09-09 09:07:36 -0400

    DISCOUNT_LIST created

--- src/main/java/com/cydeo/StaticConstants.java
+++ src/main/java/com/cydeo/StaticConstants.java
@@ -2,6 +2,7 @@ package com.cydeo;
 
 import com.cydeo.balance.Balance;
 import com.cydeo.category.Category;
+import com.cydeo.discount.Discount;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,4 +17,7 @@ public class StaticConstants {
     public static final List<Balance> BALANCE_LIST=new ArrayList<>();
 
     public static final List<Balance> GIFT_CARD_BALANCE_LIST=new ArrayList<>();
+
+    public static final List <Discount> DISCOUNT_LIST=new ArrayList<>();
+
 }
