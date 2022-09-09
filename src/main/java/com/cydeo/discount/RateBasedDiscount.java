f5d66ca27fc9fe299c19fb8bdce0f69d30be81c7
Umran Ozman <umonal@yahoo.com>
2022-09-09 09:01:32 -0400

    getter created AmountBasedDiscount Class

--- src/main/java/com/cydeo/discount/RateBasedDiscount.java
+++ src/main/java/com/cydeo/discount/RateBasedDiscount.java
@@ -6,9 +6,14 @@ public class RateBasedDiscount extends Discount{
 
     private Double rateAmount;
 
+
     public RateBasedDiscount(UUID discountId, String name, Double thresholdAmount, Double rateAmount) {
         super(discountId, name, thresholdAmount);
         this.rateAmount = rateAmount;
+    }
+
+    public Double getRateAmount() {
+        return rateAmount;
     }
 
     @Override
