5fb555087d3b3dd62e08cc95b90bbee4ce4c4610
Umran Ozman <umonal@yahoo.com>
2022-09-09 10:36:07 -0400

    Scanner objects created Customer_List printed

--- src/main/java/com/cydeo/Main.java
+++ src/main/java/com/cydeo/Main.java
@@ -1,14 +1,28 @@
 package com.cydeo;
 
+import java.util.Scanner;
+
 public class Main {
        public static void main(String[] args) {
 
 
               DataGenerator.createCustomer();
-              //createCategory();
-              //createProduct();
-              //createBalance();
-              //createDiscount();
+              DataGenerator.createCategory();
+              DataGenerator.createProducts();
+              DataGenerator.createBalance();
+              DataGenerator.createDiscount();
+
+              Scanner scanner=new Scanner(System.in);
+
+              System.out.println("Select Customer");
+
+              for(int i=0; i<StaticConstants.CUSTOMER_LIST.size(); i++){
+
+                     System.out.println("Type " + i+"for customer:"+StaticConstants.CUSTOMER_LIST.get(i).getUserName()); }
+
+
+
+              }
 
        }
 }
